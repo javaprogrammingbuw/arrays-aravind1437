7d27627e7bf1706e777c0fadbbc45e72aac44bcc
aravind1437 <44303218+aravind1437@users.noreply.github.com>
2019-03-25 13:36:46 +0530

    please check with the assignment
    In sort and search, sort was implemented but left with search.

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,9 +1,97 @@
-//Bonus 2D Arrays
-public class MagicSquare {
-	/*
-	 * Todo: A magic square is a quadratic matrix where the sums of each row and
-	 * each column are equal. All possible numbers from 1 to (side length)^2 have to occur
-	 * in the square. Write a method that takes a 2D Array as input and checks if it is a magic
-	 * square or not.
-	 * */
+public class MagicSquare{
+
+    static int rowSum1,rowSum2,rowSum3,rowSum4;
+               
+    static int columnSum1,columnSum2,columnSum3,columnSum4;
+
+               public static void main(String [] args)
+
+                 {
+
+        int square[][]={{4,5,4,8},{5,6,2,7}, {9,3,7,23},{23,3,8,3}};
+
+          // get each rows total
+        for (int i =0; i < 4;i++){
+
+        	rowSum1 += square[0][i];
+
+        }
+
+        for (int i =0; i < 4;i++){
+
+            rowSum2 += square[1][i];
+
+        }
+
+          for (int i =0; i < 4;i++){
+
+            rowSum3 += square[2][i];
+
+       }
+
+        for (int i =0; i < 4;i++){
+        	
+         rowSum4 += square[3][i];
+
+        }
+
+        // get each columns totals
+
+        for (int i =0; i < 4;i++){
+
+            columnSum1 += square[i][0];
+
+        }
+
+        for (int i =0; i < 4;i++){
+
+            columnSum2 += square[i][1];
+
+        }
+
+       for (int i =0; i < 4;i++){
+
+            columnSum3 += square[i][2];
+
+        }
+
+      for (int i =0; i < 4;i++){
+
+            columnSum4 += square[i][3];
+
+        }
+
+        // print out, just to make sure it's working correctly
+
+        System.out.println(rowSum1); 
+
+        System.out.println(rowSum2);
+
+        System.out.println(rowSum3);
+
+        System.out.println(rowSum4 + "\n");
+
+        
+
+        System.out.println(columnSum1); 
+
+        System.out.println(columnSum2);
+
+        System.out.println(columnSum3);
+
+        System.out.println(columnSum4);
+
+          if ((rowSum1 == rowSum2) && (rowSum1 == rowSum3) && (rowSum1 == rowSum4) && (rowSum1 == columnSum1)
+
+                && (rowSum1 == columnSum2) && (rowSum1 == columnSum3) && (rowSum1 == columnSum4)){
+
+            System.out.println("Magic square is =");
+
+        } else {
+
+            System.out.println("Not a magic square");
+
+        }
+
+                 }
 }
--- SortAndSearch.java
+++ SortAndSearch.java
@@ -7,4 +7,26 @@ public class SortAndSearch {
 	//First, it sorts this array by using the bubblSrt method. Then, it searches for n.
 	//You can look up the binary search algorithm here: https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
 
+		//intermediate
+			public static int[] bubbleSort(int x[]) {
+				for (int i = 0; i < x.length; i++) {
+					for (int j = 1; j < (x.length - 1); j++) {
+						if (x[j - 1] > x[j]) {
+							int a = x[j];
+							x[j] = x[j - 1];
+							x[j - 1] = a;
+						}
+					}
+				}	
+				return x;
+			}
+
+			public static void main(String[] args) {
+				int[] x = {8, 2, 6, 4, 10};
+				bubbleSort(x);
+				for (int i = 0; i < x.length; i++) {
+				System.out.print(x[i] + " ");
+				}
+			}	
+	
 }
